89f8a11ccd6c3044c17346012a1f9b9c974ab037
Liben <adugnaliben65@gmail.com>
2025-04-04 18:56:47 +0300

    Refactor main.dart to add bottom navigation and separate pages for Gallery, Videos, and Summary

--- keneniapp/lib/main.dart
+++ keneniapp/lib/main.dart
@@ -1,10 +1,74 @@
-import 'package:flutter/material.dart';
-import 'package:keneniapp/Gallery/gallery.dart';
-import 'package:keneniapp/Gallery/tiktok.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: VideoPage(),
-  ));
+import 'package:flutter/material.dart';
+import 'package:keneniapp/Gallery/about.dart';
+import 'package:keneniapp/Gallery/gallery.dart';
+import 'package:keneniapp/Gallery/tiktok.dart'; // Ensure this file exists and contains the VideoScrollPage class
+
+void main() => runApp(KeneniMemorialApp());
+
+class KeneniMemorialApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Keneni Memorial',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: Colors.black,
+        primaryColor: Colors.redAccent,
+        bottomNavigationBarTheme: BottomNavigationBarThemeData(
+          backgroundColor: Colors.black,
+          selectedItemColor: Colors.redAccent,
+          unselectedItemColor: Colors.white70,
+        ),
+      ),
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = [
+    GalleryPage(),
+    VideoPage(),
+    LifeSummaryPage(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.photo),
+            label: 'Gallery',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.video_collection),
+            label: 'Videos',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Summary',
+          ),
+        ],
+      ),
+    );
+  }
 }
 
